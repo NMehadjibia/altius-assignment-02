c1c222d8266674f23ef982f6d60853017e40b6a3
NMehadjibia <nourelhoudamehadjibia@gmail.com>
2022-04-06 01:55:51 +0100

    feat: try and catch while reading from stdin

--- index.js
+++ index.js
@@ -9,41 +9,47 @@ async function main() {
         output: stdout
     });
     const inputsIterator = readFromTerminal[Symbol.asyncIterator]();
-    
     let size;
-    do{
-        console.log("Size: ");
-        const data = await inputsIterator.next();
-        size = data.value;
-        if (!size) console.log("===>> Size is empty !");
-        else {
-            size = parseFloat(data.value);
-            if(!Number.isInteger(size)) console.log("==>> Size must be an integer");
-            else if (size <= 0) console.log("==>> Size must be greater than 0");
-        }
-    } while (!size || !Number.isInteger(size) || size <= 0);
-
     let arr;
-    do{
-        console.log("Array: ");
-        const data = await inputsIterator.next();
-        arr = data.value;
-        if(!arr) console.log("===>> Array is empty !");
-        else {
-            arr = arr.replace(/ {1,}/g, " ").split(" ");
-            const notANum = arr?.find(elm => isNaN(elm));
-            if (notANum) {
-                arr = undefined;
-                console.log("===>> Array must contain only numbers !");
-            } else {
-                if (arr.length > size) arr = arr.slice(0, size);
-                arr = arr.map(elm => parseFloat(elm));
+
+    try {
+        do{
+            console.log("Size: ");
+            const data = await inputsIterator.next();
+            size = data.value;
+            if (!size) console.log("===>> Size is empty !");
+            else {
+                size = parseFloat(data.value);
+                if(!Number.isInteger(size)) console.log("==>> Size must be an integer");
+                else if (size <= 0) console.log("==>> Size must be greater than 0");
             }
-        }
-    } while (!arr);
-    readFromTerminal.close();
-    console.log("Array: ", arr);
+        } while (!size || !Number.isInteger(size) || size <= 0);
     
+        do{
+            console.log("Array: ");
+            const data = await inputsIterator.next();
+            arr = data.value;
+            if(!arr) console.log("===>> Array is empty !");
+            else {
+                arr = arr.replace(/ {1,}/g, " ").split(" ");
+                const notANum = arr?.find(elm => isNaN(elm));
+                if (notANum) {
+                    arr = undefined;
+                    console.log("===>> Array must contain only numbers !");
+                } else {
+                    if (arr.length > size) arr = arr.slice(0, size);
+                    arr = arr.map(elm => parseFloat(elm));
+                }
+            }
+        } while (!arr);
+    } catch (error) {
+        console.log("Something went wrong while processing inputs !");
+    } finally {
+        readFromTerminal.close();
+    }
+
+    console.log("Array: ", arr);
+
     let minDistance = minimumDistances(arr);
 
     console.log("Minimum distances: ", minDistance);
@@ -51,6 +57,10 @@ async function main() {
 
 function minimumDistances(array){
     let minDistance = -1;
+
+    if(!array || !Array.isArray(array) || array.length === 0) 
+        return minDistance;
+
     let processingArr = array?.map(elm => {
         return { value: elm, checked: false};
     });
@@ -68,6 +78,7 @@ function minimumDistances(array){
                 const distance = nextIndex - currentIndex;
                 if (minDistance === -1 || minDistance > distance) minDistance = distance;
             }
+
             processingArr[currentIndex].checked = true;
         }
     }
